6c68f417445bd4aa9f539da074c93d84e9b0cf6d
Oğulcan Durkan <88947242+Ogubenn@users.noreply.github.com>
2023-05-20 15:53:54 +0300

    TMP

--- Assets/Scripts/MainMenü/VolumeSaveController.cs
+++ Assets/Scripts/MainMenü/VolumeSaveController.cs
@@ -10,9 +10,28 @@ public class VolumeSaveController : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI volumeTextUI = null;
 
+    void Start()
+    {
+        LoadValues();
+    }
+
     public void VolumeSlider(float volume)
     {
         volumeTextUI.text = volume.ToString("0.0");
+    }
+
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue",volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 
 
