86187cb584fbdbbe99b9a239855ad6bc31e119a1
Oğulcan Durkan <88947242+Ogubenn@users.noreply.github.com>
2023-05-20 23:28:43 +0300

    crash

--- Assets/Scripts/Game/Player/PlayerTrigger.cs
+++ Assets/Scripts/Game/Player/PlayerTrigger.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerTrigger : MonoBehaviour
 {
+    private Animator anim;
     private Health GettingHealt;
     public  int EnemyDamage = 5;//Turretin playera verdiği hasar
 
     void Awake()
     {
+        anim = GetComponent<Animator>();
         GettingHealt = GetComponent<Health>();
     }
 
@@ -16,7 +18,19 @@ public class PlayerTrigger : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("TurretBullet"))
+        {
             GettingHealt.TakeDamage(EnemyDamage);
+            anim.SetTrigger("isDead");
+        }
+
+        if(GettingHealt.currentHealt <= 0)
+            PlayerDead();
+    }
+
+    public void PlayerDead()
+    {
+        //gameObject.SetActive(false);
+        //Checkpoint sisteminde checkpointe geri dönüş yapılacak.
     }
 
     #endregion
